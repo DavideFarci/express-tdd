e77192e2fa9eefd57c3290cda9e239be59020ec1
DavideFarci <yank92.df@gmail.com>
2023-11-16 18:28:21 +0100

    add bonus 2

--- model.test.js
+++ model.test.js
@@ -1,10 +1,9 @@
 const { test, expect } = require("@jest/globals");
 const posts = require("./db/posts.json");
-const model = require("./model");
 const Model = require("./model");
 
 test("Model dovrebbe essere una classe", () => {
-  expect(typeof model).toBe("function");
+  expect(typeof Model).toBe("function");
 });
 
 test("L’istanza di model dovrebbe richiedere il nome del file json da usare in fase di creazione dell’istanza", () => {
