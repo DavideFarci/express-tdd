07f9a46b04a0b6b2358f61fee6c6050ca20a99f8
DavideFarci <yank92.df@gmail.com>
2023-11-16 15:19:18 +0100

    add file test, add file functions

---
+++ createSlug.test.js
@@ -0,0 +1,9 @@
+const { text, expect } = require("@jest/globals");
+const createSlug = require("./createSlug");
+
+test("Dovrebbe restituire una stringa", () => {
+  const title = "questo è il mio primo test";
+  const result = createSlug(title);
+
+  expect(result).toBe("questo-è-il-mio-primo-test");
+});
